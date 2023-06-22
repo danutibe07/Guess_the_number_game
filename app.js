4335cf5ee2092499f056c1ee64f069107001a382
danutibe07 <danielutibe07@gmail.com>
2023-06-22 23:05:12 +0100

    implemented Restart btn

--- app.js
+++ app.js
@@ -1,15 +1,39 @@
-const randomNumber = Math.ceil(Math.random() * 100) + 1;
+let randomNumber = Math.ceil(Math.random() * 100) + 1;
+let guesses = [];
 
 window.onload = function () {
   document.getElementById("submit").addEventListener("click", playGame);
+  document.getElementById("restart").addEventListener("click", restartGame);
 };
+
 
 function playGame() {
   const input = document.getElementById("num").value;
   const alertMsg = document.getElementById("status");
+  const displayGuesses = document.getElementById("history");
+  if (input === "") {
+    alertMsg.innerHTML = "Please enter a number.";
+    alertMsg.classList.add("error");
+    return; // Stop execution if input is empty
+  }
+  
   Alert(input , randomNumber , alertMsg);  
   Terms(input , alertMsg);
+  gameHistory(input, displayGuesses);
+  document.getElementById("num").value = ""; 
 }
+
+function restartGame() {
+    randomNumber = Math.ceil(Math.random() * 100) + 1; // Generate a new random number
+    guesses = []; // Reset the guesses array
+  
+    const alertMsg = document.getElementById("status");
+    const displayGuesses = document.getElementById("history");
+
+    alertMsg.innerHTML = ""; // Clear the status message
+    displayGuesses.innerHTML = ""; // Clear the guess history
+    document.getElementById("num").value = ""; // Empty the input field
+  }
 
 function Terms(input , alertMsg) {
   let texthigh = "guess is too high";
@@ -40,6 +64,14 @@ function Alert(input, randomNumber, alertMsg) {
   }
 }
 
-function History() {
-    
+function gameHistory(input , displayGuesses) {
+   guesses.push(input);
+   displayGuesses.innerHTML = ""; // Clear previous history
+
+   for (let i = 0; i < guesses.length; i++) {
+    displayGuesses.classList.add("history")
+    displayGuesses.innerHTML += `
+    <li>You guessed ${guesses[i]}</li>
+    `
+   }
 }
